33c76453079b483b1fd5ac575ad166bc6c6f67cc
VihSej <s.personal3104@gmail.com>
2025-12-08 15:54:13 -0500

    Added files for send/accept/reject request. Added files for user list return, filtered list return, recommendations list return

---
+++ roommate-matcher/src/main/java/com/cs520group8/roommatematcher/dto/MatchDTO.java
@@ -0,0 +1,5 @@
+package com.cs520group8.roommatematcher.dto;
+
+public class MatchDTO {
+
+}
---
+++ roommate-matcher/src/main/java/com/cs520group8/roommatematcher/service/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cs520group8.roommatematcher.service;
+
+public class UserServiceImpl {
+
+}
+// For returning normal user list, and filtered user list

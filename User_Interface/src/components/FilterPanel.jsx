d5a6b1178a33758d02072bcc4400f79255f251e8
VihSej <s.personal3104@gmail.com>
2025-12-09 15:41:43 -0500

    Filter feature frontend working

--- User_Interface/src/components/FilterPanel.jsx
+++ User_Interface/src/components/FilterPanel.jsx
@@ -1,108 +1,168 @@
-import './FilterPanel.css'
+import "./FilterPanel.css";
 
-function FilterPanel() {
+function FilterPanel({ filters = {}, onChange }) {
+  function handleChange(field, value, e) {
+    const updatedFilters = { ...filters };
+
+    if (e.target.checked) {
+      updatedFilters[field] = value;
+    } else {
+      updatedFilters[field] = null;
+    }
+
+    onChange(updatedFilters);
+  }
+
   return (
     <div className="filter-panel">
-      <div className="filter-instructions">
-        Filter roommates
-      </div>
+      <div className="filter-instructions">Filter roommates</div>
 
-      {/* Room Filter */}
       <div className="filter-group">
-        <h3 className="filter-title">ROOM</h3>
+        <h3 className="filter-title">SLEEP SCHEDULE</h3>
         <div className="filter-options">
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Have A Place</span>
+            <input
+              type="checkbox"
+              checked={filters.sleepSchedule === "EARLY_BIRD"}
+              onChange={handleChange("sleepSchedule", "EARLY_BIRD")}
+            />
+            <span>Early bird</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Need A Place</span>
+            <input
+              type="checkbox"
+              checked={filters.sleepSchedule === "NIGHT_OWL"}
+              onChange={handleChange("sleepSchedule", "NIGHT_OWL")}
+            />
+            <span>Night owl</span>
+          </label>
+
+          <label className="filter-option">
+            <input
+              type="checkbox"
+              checked={filters.sleepSchedule === "FLEXIBLE"}
+              onChange={handleChange("sleepSchedule", "FLEXIBLE")}
+            />
+            <span>Flexible</span>
           </label>
         </div>
       </div>
 
-      {/* When Filter */}
       <div className="filter-group">
-        <h3 className="filter-title">WHEN?</h3>
+        <h3 className="filter-title">FOOD TYPE</h3>
         <div className="filter-options">
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Fall</span>
+            <input
+              type="checkbox"
+              checked={filters.foodType === "VEGETARIAN"}
+              onChange={handleChange("foodType", "VEGETARIAN")}
+            />
+            <span>Vegetarian</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Spring</span>
+            <input
+              type="checkbox"
+              checked={filters.foodType === "NON_VEGETARIAN"}
+              onChange={handleChange("foodType", "NON_VEGETARIAN")}
+            />
+            <span>Non-vegetarian</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Winter</span>
-          </label>
-          <label className="filter-option">
-            <input type="checkbox" />
-            <span>Summer</span>
+            <input
+              type="checkbox"
+              checked={filters.foodType === "FLEXIBLE"}
+              onChange={handleChange("foodType", "FLEXIBLE")}
+            />
+            <span>Flexible</span>
           </label>
         </div>
       </div>
 
-      {/* Gender Identity Filter */}
       <div className="filter-group">
-        <h3 className="filter-title">GENDER IDENTITY</h3>
+        <h3 className="filter-title">PERSONALITY</h3>
         <div className="filter-options">
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Male</span>
+            <input
+              type="checkbox"
+              checked={filters.personalityType === "INTROVERT"}
+              onChange={handleChange("personalityType", "INTROVERT")}
+            />
+            <span>Introvert</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Female</span>
+            <input
+              type="checkbox"
+              checked={filters.personalityType === "EXTROVERT"}
+              onChange={handleChange("personalityType", "EXTROVERT")}
+            />
+            <span>Extrovert</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Other</span>
+            <input
+              type="checkbox"
+              checked={filters.personalityType === "AMBIVERT"}
+              onChange={handleChange("personalityType", "AMBIVERT")}
+            />
+            <span>Ambivert</span>
           </label>
         </div>
       </div>
 
-      {/* Smoking Filter */}
       <div className="filter-group">
         <h3 className="filter-title">SMOKING</h3>
         <div className="filter-options">
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>No</span>
+            <input
+              type="checkbox"
+              checked={filters.smokingPreference === "NO_PREFERENCE"}
+              onChange={handleChange("smokingPreference", "NO_PREFERENCE")}
+            />
+            <span>No preference</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Yes</span>
+            <input
+              type="checkbox"
+              checked={filters.smokingPreference === "PREFER_NON_SMOKER_ONLY"}
+              onChange={handleChange(
+                "smokingPreference",
+                "PREFER_NON_SMOKER_ONLY"
+              )}
+            />
+            <span>Prefer non-smoker only</span>
           </label>
         </div>
       </div>
 
-      {/* Pets Filter */}
       <div className="filter-group">
-        <h3 className="filter-title">PETS</h3>
+        <h3 className="filter-title">PETS PREFERENCE</h3>
         <div className="filter-options">
           <label className="filter-option">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={filters.petsPreference === "NO_PETS"}
+              onChange={handleChange("petsPreference", "NO_PETS")}
+            />
             <span>No pets</span>
           </label>
+
           <label className="filter-option">
-            <input type="checkbox" />
-            <span>Cat</span>
-          </label>
-          <label className="filter-option">
-            <input type="checkbox" />
-            <span>Dog</span>
-          </label>
-          <label className="filter-option">
-            <input type="checkbox" />
-            <span>Other</span>
+            <input
+              type="checkbox"
+              checked={filters.petsPreference === "OKAY_WITH_PETS"}
+              onChange={handleChange("petsPreference", "OKAY_WITH_PETS")}
+            />
+            <span>Okay with pets</span>
           </label>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default FilterPanel
+export default FilterPanel;
